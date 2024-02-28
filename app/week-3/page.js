ac7e3f6f113e9109d83fcfacc0796474a74c7b1d
kaurrajinder0 <141068515+kaurrajinder0@users.noreply.github.com>
2024-02-27 22:56:11 -0700

    week3

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import ItemList from "./item-list";
 
 export default function Page(){
     return(
-      <main className="container mx-auto mt-8">
-        <h1 className="text-3xl font-bold mb-2">Shopping List</h1>
+      <main className="bg-slate-950">
+        <h1 className="text-3xl font-bold m-3">Shopping List</h1>
         <ItemList />
       </main>
     );
