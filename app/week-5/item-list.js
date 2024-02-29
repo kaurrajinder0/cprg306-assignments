460c6219593b5ddc661f82388488773b90baa066
kaurrajinder0 <141068515+kaurrajinder0@users.noreply.github.com>
2024-02-29 00:53:32 -0700

    week-5

--- app/week-5/item-list.js
+++ app/week-5/item-list.js
@@ -1,78 +1,44 @@
-import React from 'react';
-import Item from './item';
+"use client";
 
-const ItemList = () => {
-  const items = [
-    {
-      name: "milk, 4 L 🥛",
-      quantity: 1,
-      category: "dairy",
-    },
-    {
-      name: "bread 🍞",
-      quantity: 2,
-      category: "bakery",
-    },
-    {
-      name: "eggs, dozen 🥚",
-      quantity: 2,
-      category: "dairy",
-    },
-    {
-      name: "bananas 🍌",
-      quantity: 6,
-      category: "produce",
-    },
-    {
-      name: "broccoli 🥦",
-      quantity: 3,
-      category: "produce",
-    },
-    {
-      name: "chicken breasts, 1 kg 🍗",
-      quantity: 1,
-      category: "meat",
-    },
-    {
-      name: "pasta sauce 🍝",
-      quantity: 3,
-      category: "canned goods",
-    },
-    {
-      name: "spaghetti, 454 g 🍝",
-      quantity: 2,
-      category: "dry goods",
-    },
-    {
-      name: "toilet paper, 12 pack 🧻",
-      quantity: 1,
-      category: "household",
-    },
-    {
-      name: "paper towels, 6 pack",
-      quantity: 1,
-      category: "household",
-    },
-    {
-      name: "dish soap 🍽️",
-      quantity: 1,
-      category: "household",
-    },
-    {
-      name: "hand soap 🧼",
-      quantity: 4,
-      category: "household",
+import { useState } from "react";
+import Item from './item';
+import items from './items.json';
+
+export default function ItemList () {
+  const [sortBy, setSortBy] = useState("name");
+
+
+  const handleSortChange = (sortValue) => {
+    setSortBy(sortValue);
+  };
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "name") {
+      return a.name.localeCompare(b.name);
+    } else if (sortBy === "category") {
+      return a.category.localeCompare(b.category);
     }
-  ];
+    return 0;
+  });
 
   return (
-    <ul>
-      {items.map((item, index) => (
-        <Item key={index} name={item.name} quantity={item.quantity} category={item.category} />
+    <div>
+      <button className="m-2 p-1 w-28 bg-orange-500 text-gray-50"
+        onClick={() => handleSortChange("name")}
+      >
+         Name
+      </button>
+      <button className="m-2 p-1 w-28 bg-orange-500 text-gray-50"
+        onClick={() => handleSortChange("category")}
+      >
+         Category
+      </button>
+
+      {sortedItems.map((item) => (
+        <Item key={item.id} item={item} />
       ))}
-    </ul>
+    </div>
   );
 };
 
-export default ItemList;
 
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -3,7 +3,8 @@ import ItemList from "./item-list";
 export default function Page(){
     return(
       <main className=" min-h-screen  bg-slate-950">
-        <h1 className=" text-gray-50 text-3xl font-bold  ">Shopping List</h1>
+        <h2 className=" text-gray-50 text-3xl font-bold mt-0 m-2">Shopping List</h2>
+        <label className=" text-gray-50 m-2" > Sort By:</label>
         <ItemList />
       </main>
     );
